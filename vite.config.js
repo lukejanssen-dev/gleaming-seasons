8dda55f87ddef8d1f1a5607b78bc164136c7d8e9
Luke Janssen <luke.janssen.dev@gmail.com>
2025-11-04 20:21:41 -0500

    chore: update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,16 +5,13 @@ import { defineConfig } from "vite";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-    server: {
-        allowedHosts: ["lavonne-nonfuturistic-rickie.ngrok-free.dev"],
-    },
     build: {
         rollupOptions: {
             input: {
                 main: resolve(__dirname, "index.html"),
-                gallery: resolve(__dirname, "pages/gallery.html"),
-                about: resolve(__dirname, "pages/about.html"),
-                contact: resolve(__dirname, "pages/contact.html"),
+                gallery: resolve(__dirname, "gallery/index.html"),
+                about: resolve(__dirname, "about/index.html"),
+                contact: resolve(__dirname, "contact/index.html"),
             },
         },
     },
